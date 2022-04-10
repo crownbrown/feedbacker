eec6088d0a442813355fc5465ab70ce8d78fabaf
crownbrown <sbthatcher@gmail.com>
2022-04-10 11:54:34 -0700

    finished auth flow
    Complete Google OAuth login/session/cookie/logout flow, added prod/dev keys and logic

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+app.use(
+    cookieSession({
+        // 30 days
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+
+
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ Hi: 'Ben and Eli!' });
 });
 
 const PORT = process.env.PORT || 45674;
